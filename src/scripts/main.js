df210ee432e07f7fa276e5f7f1e96767c4ab49bd
Ruslan <gnatisin.ruslan1988@gmail.com>
2025-10-30 19:01:34 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,55 @@
 'use strict';
 
 // write your code here
+
+const promise1 = new Promise((resolve, reject) => {
+  const logo = document.querySelector('.logo');
+
+  if (logo) {
+    const onClick = () => {
+      logo.removeEventListener('click', onClick);
+      resolve('Promise was resolved!');
+    };
+
+    logo.addEventListener('click', onClick);
+  } else {
+    reject(new Error('Promise not found!'));
+  }
+});
+
+// promise1.then(() => {
+//   const div = document.createElement('div');
+
+//   div.classList.add('message');
+//   div.textContent = 'Promise was resolved!';
+//   document.body.append(div);
+// });
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise was rejected!'));
+  }, 3000);
+});
+
+// promise2.catch(() => {
+//   const div = document.createElement('div');
+
+//   div.classList.add('message', 'error-message');
+//   div.textContent = 'Promise was rejected!';
+//   document.body.append(div);
+// });
+Promise.race([promise1, promise2])
+  .then((msg) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message');
+    div.textContent = msg;
+    document.body.append(div);
+  })
+  .catch((msg) => {
+    const div = document.createElement('div');
+
+    div.classList.add('message', 'error-message');
+    div.textContent = msg;
+    document.body.append(div);
+  });
